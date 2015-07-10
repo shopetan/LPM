20283ff338991857ab9ddcc753b04797484add0d
momenneko <momenneko@users.noreply.github.com>
2015-07-10 17:28:48 +0900

    検索エラーをアラートに変更

--- js/search.js
+++ js/search.js
@@ -6,31 +6,78 @@ var addDataStore = milkcocoa.dataStore('lpm/address');
 var nameTextArea, categoryArea, addTextArea, board, resultTable;
 var lpcount;
 
-//catDataStore.push({id:"4", name:"黄色い猫", category:"猫", address:"つくば市"})
-/*
-lpmDataStore.push({id:"5", name:"白猫", category:"猫", address:"牛久市"})
-lpmDataStore.push({id:"6", name:"青い傘", category:"小物", address:"つくば市"})
-*/
 
 window.onload = function(){
+  //TestDataPush();
 	nameTextArea = document.getElementById("name");
   categoryArea = document.forms.form.select;
   addrTextArea = document.getElementById("address");
   resultHeader = document.getElementById("result_p");
   resultTable = document.getElementById('search_result');
+  $("#null_alert").css("display", "none");
+  ShowAllData();
+}
+
+// milkcocoa のテストデータを push する
+function TestDataPush() {
+  lpmDataStore.push({id:"3", name:"黄色い傘", category:"傘", lat:"36.10636387079253", lng:"140.1116754859686", address:"〒305-0005 茨城県つくば市天久保４丁目５−１８"})
+}
+
+// 検索結果のテーブルをリセットする
+function resultBoardInit() {
+  resultTable.innerHTML = "";
+}
+
+// 検索条件を表示
+function addTextResult(name, cate, addr) {
+  resultHeader.innerHTML = "名前[" + name + "] カテゴリー["+ cate + "] 住所[" + addr + "]の検索結果";
+}
+
+// 検索結果のテーブルのヘッダーを表示する
+function addTableHead() {
+  var head = resultTable.insertRow(0);
+  head.className = "info";
+  var cell1 = head.insertCell(0);
+  var cell2 = head.insertCell(1);
+  var cell3 = head.insertCell(2);
+  var cell4 = head.insertCell(3);
+  var cell5 = head.insertCell(4);
+  cell1.innerHTML = "名前";
+  cell2.innerHTML = "カテゴリ";
+  cell3.innerHTML = "住所";
+  cell4.innerHTML = "緯度";
+  cell5.innerHTML = "軽度";
+}
+
+// 落し物一覧を表示する
+function ShowAllData() {
   resultHeader.innerHTML = "落し物一覧";
-  // 落し物一覧を表示する
   lpmDataStore.stream().size(20).next(function(err, lpm) {
     var count = 1;
     addTableHead();
-  	lpm.forEach(function(lp) {
-  		addText(count, lp.value.name, lp.value.category, lp.value.address);
-  	})
+    lpm.forEach(function(lp) {
+      addText(count, lp.value.name, lp.value.category, lp.value.address, lp.value.lat, lp.value.lng);
+    })
   })
 }
 
+// 検索結果の落し物の一覧を表示
+function addText(i, name, category, address, latitude, longitude){
+  var row = resultTable.insertRow(i);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  cell1.innerHTML = name;
+  cell2.innerHTML = category;
+  cell3.innerHTML = address;
+  cell4.innerHTML = latitude;
+  cell5.innerHTML = longitude;
+}
+
 // search ボタンをクリックしたとき
-function clickEvent(){
+function clickSearch(){
 	var name = nameTextArea.value;
   var cateNo = categoryArea.selectedIndex;
   var cateName = categoryArea.options[cateNo].value;
@@ -56,28 +103,16 @@ function search(cat, cateName, addr, namae){
   	lpm.forEach(function(lp) {
   		//console.log("全部-----" + lp.value.category + " " + lp.value.address);
   		//console.log(cat + addr)
-  		
-  		if(((lp.value.name).indexOf(namae) != -1 || namae === "") && (addr === lp.value.address || addr === "") && (cateName === lp.value.category || cat === 0)){
+  		if(((lp.value.name).indexOf(namae) != -1 || namae === "") && ((lp.value.address).indexOf(addr) != -1  || addr === "") && (cateName === lp.value.category || cat === 0)){
         if(lpcount === 0) {
           console.log("aaaa");
           addTableHead();
           lpcount++;
   			}
         console.log("name match" + lp.value.name + namae);
-  			addText(lpcount, lp.value.name, lp.value.category, lp.value.address);
+  			addText(lpcount, lp.value.name, lp.value.category, lp.value.address, lp.value.lat, lp.value.lng);
   			lpcount++;  			
   		}
-  		// 住所の検索
-      /*
-  		if(addr === lp.value.address) {
-  			console.log("address match" + lp.value.address);
-  			lpcount++;
-  		} else if(cat === lp.value.category) {
-  			console.log("ooooo");
-  			addText("名前: " + lp.value.name + " カテゴリ: " + lp.value.category + " 住所: " + lp.value.address);
-  			lpcount++;
-  		}
-      */
   	});
   	//検索結果が1件も見つからなかったとき
   	if(lpcount === 0) {
@@ -89,36 +124,15 @@ function search(cat, cateName, addr, namae){
 
 // どの項目も入力されていないとき
 function searchError() {
-  resultHeader.innerHTML = "少なくとも1つ以上条件を追加してください。";
+  console.log("ERROR");
+  resultBoardInit();
+  $("#null_alert").css("display", "block");
+  ShowAllData();
 }
 
-// header を表示
-function addTextResult(name, cate, addr) {
-	resultHeader.innerHTML = "名前[" + name + "] カテゴリー["+ cate + "] 住所[" + addr + "]の検索結果";
+// アラートを消す
+function clickCloseAlert() {
+  $('#null_alert').css("display", "none");
 }
 
-// 検索結果の落し物の
-function addText(i, name, category, address){
-  var row = resultTable.insertRow(i);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  cell1.innerHTML = name;
-  cell2.innerHTML = category;
-  cell3.innerHTML = address;
-}
-function resultBoardInit() {
-	resultTable.innerHTML = "";
-}
 
-function addTableHead() {
-  console.log("addTableHead");
-  var head = resultTable.insertRow(0);
-  head.className = "info";
-  var cell1 = head.insertCell(0);
-  var cell2 = head.insertCell(1);
-  var cell3 = head.insertCell(2);
-  cell1.innerHTML = "名前";
-  cell2.innerHTML = "カテゴリ";
-  cell3.innerHTML = "住所";
-}
