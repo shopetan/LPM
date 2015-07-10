7588def0b6c11e4fc5ec76fb26ed0407e65e1833
prpr-man <oharada@live.jp>
2015-07-11 01:37:54 +0900

    ログアウトを若干変更

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,17 @@
+$('.dropdown-toggle').dropdown();
+$('.dropdown-menu').find('form').click(function (e) {
+  e.stopPropagation();
+});
+
+var usr = User.getInstance();
+$(".name").text(usr.getName());
+
 $("#logout").click(function(){
   var milkcocoa = new MilkCocoa("blueib3a6u4k.mlkcca.com");
   milkcocoa.logout();
 
   window.location.href = "https://prpr-man.auth0.com/v2/logout?returnTo=http://localhost:8080/LPM/index.html";
 });
+
+
 
