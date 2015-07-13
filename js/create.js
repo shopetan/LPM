46673119bc91963f9d4309a1511ebf614dda3811
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-07-14 00:15:49 +0900

    FIX:milkccaにデータを送れるようになった．

--- js/create.js
+++ js/create.js
@@ -1,31 +1,32 @@
-$(document).ready( function(){
-	var milkcocoa = new MilkCocoa("noteibxtd2w3.mlkcca.com");
-	var lpmDataStore = milkcocoa.dataStore("LostProperty");
-	var name,category,imagePath,pickUpLatitude,pickUpLongitude,pickUpAddress;
+var milkcocoa = new MilkCocoa("noteibxtd2w3.mlkcca.com");
+var lpmDataStore = milkcocoa.dataStore("LostProperty");
+var name,category,imagePath,pickUpLatitude,pickUpLongitude,pickUpAddress;
+
+
+function clickEvent(){
 	name = document.getElementById("nameArea");
-  	category = document.getElementById("selectArea");
+	category = document.getElementById("selectArea");
 	imagePath = document.getElementById("fileInput");
 	pickUpLatitude = document.getElementById("id_lat");
 	pickUpLongitude = document.getElementById("id_lng");
 	pickUpAddress = document.getElementById("id_address");
 
-	function clickEvent(){
-		sendData(name.value,category.value,imagePath.value,pickUpLatitude.value,pickUpLongitude.value,pickUpAddress.value);
-	}
-	function sendData(name,category,imagePath,pickUpLatitude,pickUpLongitude,pickUpAddress){
-		lpmDataStore.push({
-			name:name,
-			category:category,
-			imagePath:imagePath,
-			pickUpLatitude:pickUpLatitude,
-			pickUpLongitude:pickUpLongitude,
-			pickUpAddress:pickUpAddress});
-			console.log(name.value);
-		name = "";
-		category = "";
-		imagePath = "";
-		pickUpLatitude = "";
-		pickUpLongitude = "";
-		pickUpAddress = "";
-	}
-});
+	console.log(name);
+	sendData(name.value,category.value,imagePath.value,pickUpLatitude.value,pickUpLongitude.value,pickUpAddress.value);
+}
+function sendData(name,category,imagePath,pickUpLatitude,pickUpLongitude,pickUpAddress){
+	lpmDataStore.push({
+		name:name,
+		category:category,
+		imagePath:imagePath,
+		pickUpLatitude:pickUpLatitude,
+		pickUpLongitude:pickUpLongitude,
+		pickUpAddress:pickUpAddress
+	});
+	name = "";
+	category = "";
+	imagePath = "";
+	pickUpLatitude = "";
+	pickUpLongitude = "";
+	pickUpAddress = "";
+}
