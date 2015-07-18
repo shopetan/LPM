78c15ec268562161ec161c34d3f45c22cf452292
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-07-19 04:00:24 +0900

    FIX: datastore in js/search.js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 var milkcocoa = new MilkCocoa("noteibxtd2w3.mlkcca.com");
 
-var lpmDataStore = milkcocoa.dataStore('lpm');
+var lpmDataStore = milkcocoa.dataStore('LostProperty');
 var nameTextArea, categoryArea, addressRadioArea0, addressRadioArea1, addressRadioArea2, addTextArea, board, resultTable,latArea,lngArea, addrMapArea;
 var lpcount, page, tr;
 
