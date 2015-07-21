2924d7a3be1122692843aa15a360d0e06665dd79
prpr-man <oharada@live.jp>
2015-07-21 18:05:59 +0900

    モバイル時に，ログインウィジェットを中央に配置するようにした

--- js/login.js
+++ js/login.js
@@ -6,12 +6,9 @@ $('.dropdown-menu').find('form').click(function (e) {
 var lock = new Auth0Lock('PhvoZNiNCYm0qXBVmsBXP7ZO0MJAiACe', 'shopetan.auth0.com');
 var milkcocoa = new MilkCocoa("noteibxtd2w3.mlkcca.com");
 
-lock.show({
-  container: 'root',
-  authParams: {
-    scope: 'openid profile'
-  }
-}, function (err, profile, token) {
+var width = window.innerWidth;
+
+function auth(err, profile, token) {
   milkcocoa.authWithToken(token, function(err, user) {
     if(err){
       console.log(err);
@@ -19,4 +16,22 @@ lock.show({
     }
     window.location.reload();
   });
-});
+}
+
+if(width >= 768) {
+  lock.show({
+    container: 'root',
+    authParams: {
+      scope: 'openid profile'
+    }
+  }, function (err, profile, token) {
+    auth(err, profile, token);
+  });
+}
+else {
+  $(".dropdown-toggle").click(function() {
+    lock.show({authParams: {scope: 'openid profile'}}, function (err, profile, token) {
+      auth(err, profile, token);
+    });
+  });
+}
