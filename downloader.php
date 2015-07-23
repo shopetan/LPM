efa524a589270e8d174e4847e05421a7e81e0260
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-07-24 00:38:52 +0900

    add: file_name

--- downloader.php
+++ downloader.php
@@ -1,6 +1,7 @@
 <?php
 	$url = $_POST['url'];
 	$file_name = $_POST['file_name'];
+	while (is_file($file_name = sha1(mt_rand() . microtime())));
 
 	$data = file_get_contents($url);
 	file_put_contents("./images/user_icons/{$file_name}",$data);
