c306a2b1d0d1cb5be7499bf9658ac15db9ccd9c2
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-07-24 01:05:37 +0900

    add validation

--- downloader.php
+++ downloader.php
@@ -3,6 +3,11 @@
 	$file_name = $_POST['file_name'];
 	while (is_file($file_name = sha1(mt_rand() . microtime())));
 
+	if (strpos($url, '.') === false && is_file($url)) {
 	$data = file_get_contents($url);
 	file_put_contents("./images/user_icons/{$file_name}",$data);
+	} else {
+	echo 'This file is not available';
+	}
+
 ?>
