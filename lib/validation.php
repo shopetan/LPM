c652d6c6944a38cd9d853d128a1eaf1bb2b9f33d
eshiho <shiho.elliptic@gmail.com>
2015-07-24 03:22:03 +0900

    [FIX] XSS
    Thanks to my friend.

--- lib/validation.php
+++ lib/validation.php
@@ -4,6 +4,10 @@ function is_valid_url($url) {
 }
 
 function sanitize_filename($fname) {
-	return str_replace("\0", "", basename($fname));
+	return h(str_replace("\0", "", basename($fname)));
+}
+
+function h($text) {
+	return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
 }
 ?>
